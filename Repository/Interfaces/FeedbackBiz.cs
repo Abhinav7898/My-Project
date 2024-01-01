b2bc5a92ec98d0e22c0f9296b5def5ef0fa45ae2
Abhinav Jain <107689446+Abhinav7898@users.noreply.github.com>
2024-01-01 19:47:36 +0530

    Add files via upload

---
+++ Repository/Interfaces/FeedbackBiz.cs
@@ -0,0 +1,20 @@
+﻿using CRUD.Dal;
+using CRUD.Dto;
+
+namespace CRUD.Repository.Interfaces
+{
+    public class FeedbackBiz : IFeedbackBiz
+    {
+        private readonly CRUDDalBase _db;
+        public FeedbackBiz(CRUDDalBase db)
+        {
+            _db = db;
+        }
+        public async Task<Feedback> AddFeedbackAsync(Feedback feedback)
+        {
+            var feedBack = await _db.Feedbacks.AddAsync(feedback);
+            await _db.SaveChangesAsync();
+            return feedback;
+        }
+    }
+}
